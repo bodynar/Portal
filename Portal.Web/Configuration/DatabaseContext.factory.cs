a3cb8f4cf59c50e28eeac8111d6b9f2b69a12aa8
bodynar <bodynar@gmail.com>
2020-01-19 19:27:53 +0300

    add database contect factory

---
+++ Portal.Web/Configuration/DatabaseContext.factory.cs
@@ -0,0 +1,28 @@
+﻿namespace Portal.Web
+{
+    using System.IO;
+
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+    using Microsoft.Extensions.Configuration;
+
+    using Portal.DataAccess;
+
+    public class DatabaseContextFactory : IDesignTimeDbContextFactory<DataBaseContext>
+    {
+        public DataBaseContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.Development.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<DataBaseContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            return new DataBaseContext(builder.Options);
+        }
+    }
+}
