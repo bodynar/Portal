6d74ef9aa360856ce8634497456c6c7ec87ab1b1
bodynar <bodynar@gmail.com>
2020-04-05 21:14:33 +0300

    add addComment command

---
+++ Portal.Cqrs.Commands/Comments/AddComment/AddCommentCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Portal.Cqrs.Commands
+{
+    using Portal.Cqrs.Command;
+
+    public class AddCommentCommand : ICommand
+    {
+        public long MediaTypeId { get; set; }
+
+        public long MediaObjectId { get; set; }
+
+        public string Comment { get; set; }
+
+        public long? ParentCommentId { get; set; }
+    }
+}
---
+++ Portal.Cqrs.Commands/Comments/AddComment/AddCommentCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace Portal.Cqrs.Commands
+{
+    using Portal.Cqrs.Command;
+    using Portal.DataAccess;
+    using Portal.Entities;
+    using Portal.Infrastructure;
+
+    internal class AddCommentCommandHandler : BaseCommandHandler<AddCommentCommand>
+    {
+        private IRepository<Comment> Repository { get; }
+
+        public AddCommentCommandHandler(IResolver resolver) 
+            : base(resolver)
+        {
+            Repository = GetRepository<Comment>();
+        }
+
+        public override void Handle(AddCommentCommand command)
+        {
+            Repository.Add(
+                new Comment { 
+                    MediaObjectId = command.MediaObjectId,
+                    Text = command.Comment,
+                    ParentCommentId = command.ParentCommentId,
+                    MediaTypeId = command.MediaTypeId,
+                }
+            );
+        }
+    }
+}
