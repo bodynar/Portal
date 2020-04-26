9db4e3d6e89fb8e88828e19a674cea434487bbef
bodynar <bodynar@gmail.com>
2020-04-26 20:24:29 +0300

    update base entity

--- Portal.Entities/Base/Entity.cs
+++ Portal.Entities/Base/Entity.cs
@@ -1,7 +1,17 @@
+using System;
+
 namespace Portal.Entities
 {
     public class Entity
     {
         public long Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public long CreatedBy { get; set; }
+
+        public DateTime ModifiedOn { get; set; }
+
+        public long ModifiedBy { get; set; }
     }
 }
